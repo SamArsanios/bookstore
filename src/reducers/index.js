d3360f1262146df58f0eb011c639f10afe133538
wandji20 <wbertrand10@gmail.com>
2021-07-01 13:45:35 +0100

    Add redux chrome extension and import related functions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import booksReducer from './booksReducer';
 
 const rootReducer = combineReducers({ booksReducer });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
