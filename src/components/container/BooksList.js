1948f453582ee2d768e4f2fe9390e1036e29c2b5
SamArsanios <samson.kibrom@hotmail.com>
2021-07-01 13:24:15 +0300

    Implement App and BooksList

--- src/components/container/BooksList.js
+++ src/components/container/BooksList.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const BooksList = () => (
-  <div />
+  <div>
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+          <h5>Book ID</h5>
+        </div>
+        <div className="col-3">
+          <h5>Title</h5>
+        </div>
+        <div className="col-3">
+          <h5>Category</h5>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default BooksList;
