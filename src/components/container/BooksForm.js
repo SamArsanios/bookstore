03e879ca4181691a137358249e52c2c2b885d8c3
wandji20 <wbertrand10@gmail.com>
2021-07-02 09:45:53 +0100

    Add handleChange methods for title and category

--- src/components/container/BooksForm.js
+++ src/components/container/BooksForm.js
@@ -1,26 +1,48 @@
-import React from 'react';
+// /* eslint-disable */
+import React, { useState } from 'react';
 
-const BooksForm = () => (
-  <div>
-    <div className="mb-3">
-      <label htmlFor="exampleFormControlInput1" className="form-label">
-        Book Title
-        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Title" />
-      </label>
+const BooksForm = () => {
+  const categories = ['Action', 'Biography', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+    console.log(title);
+  };
+  const handleCategoryChange = (e) => {
+    // console.log(e.target.value);
+    setCategory(e.target.value);
+  };
+  console.log(['category:', category, 'k']);
+  return (
+    <div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">
+          Book Title
+          <input onChange={handleTitleChange} value={title} type="text" className="form-control" id="exampleFormControlInput1" placeholder="Title" />
+        </label>
+      </div>
+
+      <select
+        onChange={handleCategoryChange}
+        className="form-select"
+        aria-label="Default select example"
+      >
+        {
+          categories.map((category) => (
+            <option
+              key={category}
+              value={category}
+            >
+              {category}
+            </option>
+          ))
+        }
+      </select>
+
+      <button type="button" className="btn btn-dark">Submit</button>
     </div>
-
-    <select className="form-select" aria-label="Default select example">
-      <option selected>Open this select menu</option>
-      <option value="1">Action</option>
-      <option value="2">Biography</option>
-      <option value="3">Horror</option>
-      <option value="4">Kids</option>
-      <option value="5">Learning</option>
-      <option value="6">Sci-Fi</option>
-    </select>
-
-    <button type="button" className="btn btn-dark">Submit</button>
-  </div>
-);
+  );
+};
 
 export default BooksForm;
