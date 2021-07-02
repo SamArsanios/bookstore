7bdddf28c30a843a08dee1d46a0845f0e67569fa
SamArsanios <samson.kibrom@hotmail.com>
2021-07-02 15:56:56 +0300

    Create the CHANGE_FILTER action in actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
+const CHANGE_FILTER = 'CHANGE_FILTER';
 
 const createBook = (book) => ({
   type: CREATE_BOOK,
@@ -11,6 +12,11 @@ const removeBook = (id) => ({
   payload: id,
 });
 
+const changeFilter = (id) => ({
+  type: CHANGE_FILTER,
+  payload: id,
+});
+
 export {
-  CREATE_BOOK, REMOVE_BOOK, createBook, removeBook,
+  CREATE_BOOK, REMOVE_BOOK, CHANGE_FILTER, createBook, removeBook, changeFilter,
 };
