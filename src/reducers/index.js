dd1ef9f0b50283bd4f59b934b87baea3f0287ad0
SamArsanios <samson.kibrom@hotmail.com>
2021-07-02 15:58:02 +0300

    add changefilter educer to comine

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,9 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import booksReducer from './booksReducer';
+import changeFilterReducer from './filterReducer';
 
-const rootReducer = combineReducers({ booksReducer });
+const rootReducer = combineReducers({ booksReducer, changeFilterReducer });
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
