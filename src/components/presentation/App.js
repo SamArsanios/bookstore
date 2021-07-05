35bdd752f263d0e24553082f95fb5a81e98916f8
SamArsanios <samson.kibrom@hotmail.com>
2021-07-05 14:19:02 +0300

    Add style to Book page

--- src/components/presentation/App.js
+++ src/components/presentation/App.js
@@ -1,10 +1,12 @@
 import '../../App.css';
 import BooksList from '../container/BooksList';
 import BooksForm from '../container/BooksForm';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <BooksList />
       <BooksForm />
     </div>
--- src/components/presentation/Book.js
+++ src/components/presentation/Book.js
@@ -6,16 +6,29 @@ const Book = (props) => {
   return (
     <div className="row mt-3">
       <div className="col-3">
-        <h5>{book.ID}</h5>
+        <div className="first">
+          <h6>{book.category}</h6>
+          <h4>{book.title}</h4>
+          <p>Author</p>
+        </div>
+        <div className="second">
+          <button className="btn" type="button">Comments</button>
+          <button className="btn" type="button" onClick={() => handleRemoveBook(book)}>Remove</button>
+          <button className="btn" type="button">Edit</button>
+        </div>
+
+        {/* <h5>{book.ID}</h5> */}
       </div>
       <div className="col-3">
-        <h5>{book.title}</h5>
+        <div className="oval-2">
+          aom
+        </div>
       </div>
       <div className="col-3">
-        <h5>{book.category}</h5>
+        {/* <h5>{book.category}</h5> */}
       </div>
       <div className="col-2">
-        <button onClick={() => handleRemoveBook(book)} type="button" className="btn btn-danger">Remove Book</button>
+        {/* sd */}
       </div>
     </div>
   );
